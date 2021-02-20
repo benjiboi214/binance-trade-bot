8db892915bd9719a5719142c5c12811e987089a7
Ben Elliot <belliot@liveperson.com>
2021-02-20 20:55:06 +1100

    Delete file ops

--- app/file_operations.py
+++
@@ -1,6 +0,0 @@
-class FileOperation():
-    
-    @staticmethod
-    def write(filename, mode, content, write_function):
-        with open(filename, mode) as open_file:
-            write_function(open_file, content)
