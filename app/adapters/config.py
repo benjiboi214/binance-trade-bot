b13e9c243722f862177ed2ff2d465caa78389005
Ben Elliot <belliot@liveperson.com>
2021-02-21 17:17:21 +1100

    Add docstring Use formatter to clean up style

--- app/adapters/config.py
+++ app/adapters/config.py
@@ -1,13 +1,19 @@
 import os, configparser, pickle
 
+
 class ConfigNotFound(Exception):
     pass
 
-class ConfigAdapter():
+
+class ConfigAdapter:
+    """
+    Abstraction for config management and Exception handling
+    """
+
     @staticmethod
     def deep_copy_config(config):
         """
-        Deep copy the config parser object for cases where 
+        Deep copy the config parser object for cases where
         the local config and this object must be different.
         """
         raw_config = pickle.dumps(config)
@@ -19,7 +25,7 @@ class ConfigAdapter():
         TODO - describe this
         """
         return configparser.ConfigParser(interpolation=interpolation)
-    
+
     @staticmethod
     def read_config_from_file(file_name, parser=None):
         """
@@ -35,29 +41,32 @@ class ConfigAdapter():
         config_path = os.path.join(config_dir, config_name)
 
         if not os.path.exists(config_path):
-            print("No configuration file named {0} found in directory {1}".format(
-                config_dir, config_name
-            ))
+            print(
+                "No configuration file named {0} found in directory {1}".format(
+                    config_dir, config_name
+                )
+            )
             exit()
 
         self.config_path = config_path
         self.parser = ConfigAdapter.read_config_from_file(self.config_path)
         print("Config successfully initialised from {0}".format(config_path))
-    
+
     @property
     def parser(self):
         return ConfigAdapter.deep_copy_config(self.__parser)
-    
+
     @parser.setter
     def parser(self, parser):
         self.__parser = parser
-    
+
     def get(self, *args, **kwargs):
         try:
             return self.__parser.get(*args, **kwargs)
         except Exception as e:
-            raise ConfigNotFound("No configuration for '{}' in section '{}'".format(args[0], args[1]))
+            raise ConfigNotFound(
+                "No configuration for '{}' in section '{}'".format(args[0], args[1])
+            )
 
     def getboolean(self, *args, **kwargs):
         return self.__parser.getboolean(*args, **kwargs)
-
