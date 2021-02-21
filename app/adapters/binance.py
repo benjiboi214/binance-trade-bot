82d41eff9944449546fdb1425af47b52736b7675
Ben Elliot <belliot@liveperson.com>
2021-02-21 17:17:31 +1100

    Formatter to cleanup style

--- app/adapters/binance.py
+++ app/adapters/binance.py
@@ -1,22 +1,20 @@
 from binance.client import Client as BinanceClient
 
 
-class ExchangeClientAdapter():
-    '''
+class ExchangeClientAdapter:
+    """
     Abstraction to organise the calls to the binance client.
     TODO - Document the interface
     TODO - Make this an abstract class
     TODO - Move Binance implementation to a concrete class
-    '''
+    """
 
     CONFIG_SECTION_NAME = "binance-client"
 
     def __init__(self, config, logger):
         self.__logger = logger
-        api_key = config.get(
-            self.CONFIG_SECTION_NAME, "api_key")
-        api_secret_key = config.get(
-            self.CONFIG_SECTION_NAME, "api_secret_key")
+        api_key = config.get(self.CONFIG_SECTION_NAME, "api_key")
+        api_secret_key = config.get(self.CONFIG_SECTION_NAME, "api_secret_key")
         self.client = BinanceClient(api_key, api_secret_key)
         self.__logger.debug("ExchangeClient successfully initialised")
 
