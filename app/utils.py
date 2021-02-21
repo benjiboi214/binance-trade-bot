8b1f010f27145b166114cf73a83db10f34ee59d8
Ben Elliot <belliot@liveperson.com>
2021-02-21 16:17:12 +1100

    Add read function Move common json dump/read to static method Add docstrings

--- app/utils.py
+++ app/utils.py
@@ -1,9 +1,14 @@
+import json
+
 from types import SimpleNamespace
 
 
-# For nested dot notation dicts
-# https://stackoverflow.com/a/54332748/5680716
 class NestedNamespace(SimpleNamespace):
+    '''
+    Used for achieving JS-like dot notated dicts in python.
+    implementation pulled from: https://stackoverflow.com/a/54332748/5680716
+    '''
+
     def __init__(self, dictionary, **kwargs):
         super().__init__(**kwargs)
         for key, value in dictionary.items():
@@ -14,12 +19,30 @@ class NestedNamespace(SimpleNamespace):
 
 
 class FileOperations():
+    '''
+    Mostly static based class used for common file operations
+    '''
     MODES = NestedNamespace({
-        "WRITE": "w"
+        "WRITE": "w",
+        "READ": "r"
     })
-    
+
+    @staticmethod
+    def read_json(open_file):
+        return json.load(open_file)
+
+    @staticmethod
+    def write_json(open_file, content):
+        json.dump(content, open_file)
+
     @staticmethod
     def write(filename, content, write_function):
         access_mode = FileOperations.MODES.WRITE
         with open(filename, access_mode) as open_file:
             write_function(open_file, content)
+
+    @staticmethod
+    def read(filename, read_function):
+        access_mode = FileOperations.MODES.READ
+        with open(filename, access_mode) as open_file:
+            return read_function(open_file)
