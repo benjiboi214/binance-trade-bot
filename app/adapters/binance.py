13d3a98b243ab73ea53002a37278d793bc380418
Ben Elliot <belliot@liveperson.com>
2021-02-21 18:08:35 +1100

    Move to new config structure

--- app/adapters/binance.py
+++ app/adapters/binance.py
@@ -1,7 +1,9 @@
 from binance.client import Client as BinanceClient
 
+from adapters.config import ConfigAccessor
 
-class ExchangeClientAdapter:
+
+class ExchangeClientAdapter(ConfigAccessor):
     """
     Abstraction to organise the calls to the binance client.
     TODO - Document the interface
@@ -9,14 +11,23 @@ class ExchangeClientAdapter:
     TODO - Move Binance implementation to a concrete class
     """
 
+    # Strings
     CONFIG_SECTION_NAME = "binance-client"
+    CONFIG_API_KEY = "api_key"
+    CONFIG_API_SECRET_KEY = "api_secret_key"
+
+    LOG_SUCCESS_MESSAGE = "ExchangeClient successfully initialised"
 
     def __init__(self, config, logger):
+        super().__init__()
+        self._save_config(config)
         self.__logger = logger
-        api_key = config.get(self.CONFIG_SECTION_NAME, "api_key")
-        api_secret_key = config.get(self.CONFIG_SECTION_NAME, "api_secret_key")
+
+        api_key = self._get_config(ExchangeClientAdapter.CONFIG_API_KEY)
+        api_secret_key = self._get_config(ExchangeClientAdapter.CONFIG_API_SECRET_KEY)
         self.client = BinanceClient(api_key, api_secret_key)
-        self.__logger.debug("ExchangeClient successfully initialised")
+
+        self.__logger.debug(ExchangeClientAdapter.LOG_SUCCESS_MESSAGE)
 
     def get_all_tickers(self):
         return self.client.get_all_tickers()
